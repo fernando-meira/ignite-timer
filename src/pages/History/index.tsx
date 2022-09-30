abcb74ea85a39e4453b9719ae5b81a6b0ee56ebd
Fernando Meira <fernando.meira@goomer.com.br>
2022-09-29 22:56:34 -0300

    feat: History Page.

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,5 +1,33 @@
 import * as S from './styles';
 
 export function History() {
-  return <S.Container>History</S.Container>;
+  return (
+    <S.HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <S.HistoryList>
+        <S.Table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {Array.from({ length: 8 }).map((_, index) => (
+              <tr key={index}>
+                <td>Tarefa</td>
+                <td>20 minutos</td>
+                <td>Há 2 meses</td>
+                <td>Concluído</td>
+              </tr>
+            ))}
+          </tbody>
+        </S.Table>
+      </S.HistoryList>
+    </S.HistoryContainer>
+  );
 }
--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -1,3 +1,68 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.main``;
+export const HistoryContainer = styled.main`
+  padding: 5.6rem;
+
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    font-size: 2.4rem;
+    color: ${({ theme }) => theme.colors.gray[100]};
+  }
+`;
+
+export const HistoryList = styled.div`
+  margin-top: 3.2rem;
+
+  flex: 1;
+
+  overflow: auto;
+`;
+
+export const Table = styled.table`
+  ${({ theme }) => css`
+    width: 100%;
+    min-width: 600px;
+    border-collapse: collapse;
+
+    th {
+      padding: 1.6rem;
+
+      line-height: 1.6;
+      text-align: left;
+      font-size: 1.4rem;
+      color: ${theme.colors.gray[100]};
+      background-color: ${theme.colors.gray[600]};
+
+      &:first-child {
+        padding-right: 2.4rem;
+        border-top-left-radius: 8px;
+      }
+
+      &:last-child {
+        padding-left: 2.4rem;
+        border-top-right-radius: 8px;
+      }
+    }
+
+    td {
+      padding: 1.6rem;
+      border-top: 4px solid ${theme.colors.gray[700]};
+
+      line-height: 1.6;
+      font-size: 1.4rem;
+      background-color: ${theme.colors.gray[700]};
+
+      &:first-child {
+        width: 50%;
+        padding-right: 2.4rem;
+      }
+
+      &:last-child {
+        padding-left: 2.4rem;
+      }
+    }
+  `}
+`;
